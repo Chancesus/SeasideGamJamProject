0629ecd1e0fec953abe898ed4f6efb917d76d83e
Chancesus <chancekgsmith@gmail.com>
2022-06-10 09:06:49 -0700

    PlayerController
    Added Player Controller Script, able to move, flip sprite, and jump once while grounded

--- Unity Project/My project/Assets/Scripts/PlayerController.cs
+++ Unity Project/My project/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float jumpVelocity = 10;
+    [SerializeField] float moveSpeed = 10f;
+    SpriteRenderer playerSprite;
+
+    [SerializeField] Transform feet;
+    [SerializeField] LayerMask groundLayer;
+
+
+
+    float horizontal;
+    Rigidbody2D playerRigid;
+    
     void Start()
     {
+        playerRigid = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
+    }
+
+   
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        FlipSprite();
+        IsGrounded();
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Jump();
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool IsGrounded()
     {
-        
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayer);
+        if (groundCheck)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void FixedUpdate() => playerRigid.velocity = new Vector2(horizontal * moveSpeed, playerRigid.velocity.y);
+
+    private void Jump()
+    {
+        Vector2 jump = new Vector2(playerRigid.velocity.x, jumpVelocity);
+        playerRigid.velocity = jump;
+    }
+
+    private void FlipSprite()
+    {
+        if (playerRigid.velocity.x < 0)
+        {
+            playerSprite.flipX = true;
+        }
+        else
+        {
+            playerSprite.flipX = false;
+        }
     }
 }
